df8a5ff48b7fe05533019f44f7c090fc11da7015
jamaludeen750@gmail.com <jamal@MyPC.>
2026-08-07 12:51:53 +0000

    Add Hero and AiTools in components folder

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Navbar } from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <Navbar />
+            <Hero />
+            <AiTools />
+        </>
     )
 }
 
