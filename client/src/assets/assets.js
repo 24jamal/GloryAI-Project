c9996816d385214cea282fdfec25aede335de728
jamaludeen750@gmail.com <jamal@MyPC.>
2026-08-09 14:38:45 +0000

    Rebranding from GloryAI to AlchemistAI

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -9,7 +9,7 @@ import { SquarePen, Hash, Image, Eraser, Scissors, FileText } from 'lucide-react
 import ai_gen_img_1 from "./ai_gen_img_1.png";
 import ai_gen_img_2 from "./ai_gen_img_2.png";
 import ai_gen_img_3 from "./ai_gen_img_3.png";
-import gloryAI_violet from "./gloryAI_violet.png";
+import alchemist_ai_logo from "./alchemist_ai_logo.png";
 
 export const assets = {
     logo,
@@ -19,7 +19,7 @@ export const assets = {
     star_dull_icon,
     profile_img_1,
     arrow_icon,
-    gloryAI_violet,
+    alchemist_ai_logo,
 };
 
 export const AiToolsData = [
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
         <footer className="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500" >
             <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
                 <div className="md:max-w-96">
-                    <img className='h-9' src={assets.gloryAI_violet} alt="Sample Logo" />
+                    <img className='h-9' src={assets.alchemist_ai_logo} alt="Sample Logo" />
                     <p className="mt-6 text-sm">
-                        GloryAI is an AI-powered platform dedicated to helping individuals and businesses create, enhance, and optimize content through intelligent automation. We build innovative tools that make advanced artificial intelligence simple, accessible, and impactful for everyone.
+                        AlchemistAI is an AI-powered platform dedicated to helping individuals and businesses create, enhance, and optimize content through intelligent automation. We build innovative tools that make advanced artificial intelligence simple, accessible, and impactful for everyone.
 
                     </p>
                 </div>
@@ -35,7 +35,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="pt-4 text-center text-xs md:text-sm pb-5">
-                Copyright 2026 © <a href="https://prebuiltui.com">GloryAI</a>. All Right Reserved.
+                Copyright 2026 © <a href="https://prebuiltui.com">AlchemistAI</a>. All Right Reserved.
             </p>
         </footer >
     );
