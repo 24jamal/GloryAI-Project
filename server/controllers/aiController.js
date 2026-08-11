bd18814085a4ebd67236e6275f58b5c6f203ea06
jamaludeen750@gmail.com <jamal@MyPC.>
2026-08-11 12:06:34 +0000

    Add generateImage api function

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -2,6 +2,7 @@ import OpenAI from "openai";
 import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
 import axios from "axios";
+import { v2 as cloudinary } from "cloudinary";
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -81,13 +82,13 @@ export const generateBlogTitle = async (req, res) => {
             ],
 
             temperature: 0.7,
-            max_tokens: 100
+            max_tokens: 200
         });
 
         const content = response.choices[0].message.content
 
         await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES 
-        (${userId},${prompt},${content},'article')`;
+        (${userId},${prompt},${content},'blog-title')`;
 
         if (plan != 'premium') {
             await clerkClient.users.updateUserMetadata(userId, {
@@ -107,4 +108,42 @@ export const generateBlogTitle = async (req, res) => {
     }
 }
 
+
+
+export const generateImage = async (req, res) => {
+
+    try {
+
+        const { userId } = req.auth();
+        const { prompt, publish } = req.body;
+        const plan = req.plan;
+
+        if (plan !== 'premium') {
+            return res.json({ success: false, message: "This feature is only available for premium subscriptions" })
+        }
+
+        const formData = new FormData()
+        formData.append('prompt', 'shot of vaporwave fashion dog in miami')
+        const { data } = await axios.post("https://clipdrop-api.co/text-to-image/v1", formData, {
+            headers: { 'x-api-key': process.env.CLIPDROP_API_KEY, },
+            responseType: 'arraybuffer'
+        })
+
+        const base64Image = `data:image/png;base64,${Buffer.from(data, 'binary').toString('base64')}`;
+
+        const { secure_url } = await cloudinary.uploader.upload(base64Image);
+
+        await sql`INSERT INTO creations (user_id, prompt, content, type, publish) VALUES
+            (${userId}, ${prompt}, ${secure_url}, 'image', ${publish ?? false})`;
+
+
+        res.json({ success: true, content: secure_url });
+    }
+
+    catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
+
+    }
+}
 
